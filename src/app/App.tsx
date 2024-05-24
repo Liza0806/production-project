9358330154266c8324e1bf13d00c5e2219acad62
Liza <fildepiorsta@gmail.com>
2024-05-24 14:08:14 +0300

    Link + theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,17 +6,17 @@ import {classNames} from "shared/lib/classNames/classNames";
 import AboutPage from "Pages/AboutPage/ui/AboutPage";
 import MainPage from "Pages/MainPage/ui/MainPage";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/NavBar";
 
 
 
 const App = () => {
 const { theme, toggleTheme } = useTheme()
     return (
-        <div className={classNames('app', {},[theme, 'cls2, cls3'])}>
+        <div className={classNames('app', {}, [theme, 'cls2, cls3'])}>
+            <Navbar/>
             <button onClick={toggleTheme}>toggle</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
-<AppRouter/>
+            <AppRouter/>
         </div>
     );
 };
