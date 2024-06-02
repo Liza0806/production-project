6850109bb198ab9964daf68ed722771f6c601d09
Liza <fildepiorsta@gmail.com>
2024-06-02 15:39:08 +0300

    smth new

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -4,8 +4,11 @@ import { FC } from 'react';
 import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
+    // eslint-disable-next-line no-unused-vars
     PRIMARY = 'primary',
+    // eslint-disable-next-line no-unused-vars
     SECONDARY = 'secondary',
+    // eslint-disable-next-line no-unused-vars
     RED = 'red'
 }
 
