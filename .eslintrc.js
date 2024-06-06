d84c4ee633f799a112af4871c766b1a7f3ecdd1e
Liza <fildepiorsta@gmail.com>
2024-06-06 19:05:40 +0300

    docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,7 +46,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to'],
+                ignoreAttribute: ['data-testid', 'to', 'aria-label'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
