3ca40df84b21f83f845e22409d628f09f79df557
Liza <fildepiorsta@gmail.com>
2024-08-10 17:24:42 +0300

    redirects4

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,11 +13,11 @@ export default (env: BuildEnv) => {
         buildLocales: path.resolve(__dirname, 'build', 'locales'),
     };
 
-    const mode = env.mode || 'development';
+    const mode = 'production';
     const PORT = env.port || 3000;
     const apiUrl = 'https://production-project-server-sepia-sigma.vercel.app';
 
-    const isDev = mode === 'development';
+    const isDev = false; // mode === 'development';
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
