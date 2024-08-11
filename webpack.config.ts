18ee3f9b5f5e3970529576d70ecd3129b7da64f0
Liza <fildepiorsta@gmail.com>
2024-08-11 12:27:07 +0300

    test3

--- webpack.config.ts
+++ webpack.config.ts
@@ -15,7 +15,7 @@ export default (env: BuildEnv) => {
 
     const mode = env.mode || 'development';
     const PORT = env.port || 3000;
-    const apiUrl = env.apiUrl || 'http://localhost:8000';
+    const apiUrl = 'production-project-server-sepia-sigma.vercel.app'; /// env.apiUrl || 'http://localhost:8000';
 
     const isDev = mode === 'development';
 
@@ -24,7 +24,7 @@ export default (env: BuildEnv) => {
         paths,
         isDev,
         port: PORT,
-        apiUrl,
+        apiUrl: 'production-project-server-sepia-sigma.vercel.app',
         project: 'frontend',
     });
 
