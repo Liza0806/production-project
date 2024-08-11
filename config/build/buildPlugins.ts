0ba2d09d73152f8f8ae1603bf8b329791c29d89f
Liza <fildepiorsta@gmail.com>
2024-08-11 11:34:34 +0300

    change api url

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -20,7 +20,7 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-            __API__: JSON.stringify('production-project-server-sepia-sigma.vercel.app'),
+            __API__: JSON.stringify(''),
             __PROJECT__: JSON.stringify(project),
         }),
         new CopyPlugin({
--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
-    baseURL: __API__,
+    baseURL: 'production-project-server-sepia-sigma.vercel.app',
     headers: {
         authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
     },
